26bb08fd0c70df6931ca7e66ac69db1adb8065c8
Pascal Jungblut <mail@pascal-jungblut.com>
2017-06-20 18:06:23 +0200

    Add more instrumentation modes
    - ctor
    - dtor
    - generic method call
    - reference with write operations
    
    Write operations currently only handle a small subset of all cases but
    for some trivial examples it works.

--- lib/Transforms/DataInstrumentation/Instrumenter.cpp
+++ lib/Transforms/DataInstrumentation/Instrumenter.cpp
@@ -1,6 +1,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
@@ -11,6 +12,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/ADT/Statistic.h"
+#include <set>
 
 #ifndef DEBUG_TYPE
 #define DEBUG_TYPE "data-instrumentation"
@@ -22,9 +24,8 @@ STATISTIC(InstrRetPts, "Number of instrumented return instructions");
 using namespace llvm;
 
 namespace {
-enum Mode { m_default = 0, read, write };
+enum Mode { m_default = 0, reference, ctor, dtor };
 
-// actual function pass
 class DataInstrumenter : public FunctionPass {
  public:
   static char ID;
@@ -42,52 +43,149 @@ class DataInstrumenter : public FunctionPass {
       // build params
       std::vector<Value*> params;
       params.push_back(target);
-      params.push_back(ConstantInt::get(Type::getInt8Ty(F.getContext()), 1 << mode, false));
-
-      // add accessed element
-      if (mode == Mode::read || mode == Mode::write) {
-        auto args = F.arg_begin();
-        auto accessed = ++args;
-        params.push_back(accessed);
-      }
-
-      auto &Ctx = entryInstr->getContext();
 
       // instrument entry point
-      FunctionType *FT;
-      Value *invoke;
-      if (mode == Mode::read) {
-       FT = FunctionType::get(Type::getVoidTy(Ctx), {target->getType(), Type::getInt8Ty(Ctx), Type::getInt64Ty(Ctx)}, false);
-       invoke = cast<Value>(entryInstr->getModule()->getOrInsertFunction("_access_log", FT));
-      } else {
-       FT = FunctionType::get(Type::getVoidTy(Ctx), {target->getType(), Type::getInt8Ty(Ctx)}, false);
-       invoke = cast<Value>(entryInstr->getModule()->getOrInsertFunction("_entry_log", FT));
-      }
-      DEBUG(errs() << "Instrumenting before entry instruction" << entryInstr << '\n');
-      InstrEntryPts++;
-      CallInst::Create(invoke, params, "", entryInstr);
-
-      // log before any return of a flagged method
-      for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-        if (isa<ReturnInst>(*I)) {
-          auto returnLog = cast<Value>(entryInstr->getModule()->getOrInsertFunction("_exit_log", FT));
-          DEBUG(errs() << "Instrumenting before return instruction" << &*I << '\n');
-          InstrRetPts++;
-          CallInst::Create(returnLog, params, "", &*I);
+      switch (mode) {
+        case Mode::reference: {
+          // add accessed element
+          auto args = F.arg_begin();
+          auto accessed = ++args;
+          params.push_back(accessed);
+          CallInst::Create(referenceInstrumentation(F, target, entryInstr, accessed), params, "", entryInstr);
+          break;
+        }
+        case Mode::ctor: {
+          CallInst::Create(ctorInstrumentation(F, target, entryInstr), params, "", entryInstr);
+          break;
+        }
+        case Mode::dtor: {
+          CallInst::Create(dtorInstrumentation(F, target, entryInstr), params, "", entryInstr);
+          break;
+        }
+        default: {
+          CallInst::Create(defaultInstrumentation(F, target, entryInstr), params, "", entryInstr);
+          break;
         }
       }
+      DEBUG(errs() << "Instrumenting before instruction" << entryInstr << '\n');
+      InstrEntryPts++;
     }
     return modified;
   }
 private:
   Mode getMode(const Attribute attr) {
     auto val = attr.getValueAsString();
-    if (val == "read") {
-      return Mode::read;
-    } else if (val == "write") {
-      return Mode::write;
+    if (val == "reference") {
+      return Mode::reference;
+    } else if (val == "ctor") {
+        return Mode::ctor;
+    } else if (val == "dtor") {
+        return Mode::dtor;
     }
     return Mode::m_default;
+  }
+  
+  Value* defaultInstrumentation(Function& f, Value *target, Instruction* i) {
+    auto &ctx = i->getContext();
+    auto *ft = FunctionType::get(Type::getVoidTy(ctx), {target->getType()}, false);
+    return cast<Value>(i->getModule()->getOrInsertFunction("_entry_log", ft));
+  }
+
+  Value* ctorInstrumentation(Function& f, Value *target, Instruction* i) {
+    auto &ctx = i->getContext();
+    auto *ft = FunctionType::get(Type::getVoidTy(ctx), {target->getType()}, false);
+    return cast<Value>(i->getModule()->getOrInsertFunction("_ctor_log", ft));
+  }
+
+  Value* dtorInstrumentation(Function& f, Value *target, Instruction* i) {
+    auto &ctx = i->getContext();
+    auto *ft = FunctionType::get(Type::getVoidTy(ctx), {target->getType()}, false);
+    return i->getModule()->getOrInsertFunction("_dtor_log", ft);
+  }
+
+  Value* writeInstrumentation(Function& f, Value *target, Instruction* i, Value *accessed) {
+    auto &ctx = i->getContext();
+    auto *ft = FunctionType::get(Type::getVoidTy(ctx), {target->getType()}, false);
+    return i->getModule()->getOrInsertFunction("_write_log", ft);
+  }
+
+  
+  /**
+   Instrument a "reference" instruction
+   
+   This instruction represents a function call that returns a reference.
+
+   @param f Current function scope
+   @param target target instruction
+   @param i The actual reference instruction
+   @param accessed The index of the accessed element
+   @return Instrumentation call
+   */
+  Value* referenceInstrumentation(Function& f, Value *target, Instruction* i, Value *accessed) {
+    // get invokations
+    for (auto user: f.users()) {
+      auto invoke = dyn_cast<InvokeInst>(user);
+      if (!invoke) continue;
+      // get accessed element
+      auto vector = invoke->getArgOperand(0);
+      auto element = invoke->getArgOperand(invoke->getNumArgOperands() - 1);
+      // get results of invokations
+      for(auto user: findWriteInstructions(invoke)) {
+        user->dump();
+        if (auto useInstr = dyn_cast<Instruction>(user)) {
+          auto &ctx = user->getContext();
+          auto params = {vector, element};
+          auto *ft = FunctionType::get(Type::getVoidTy(ctx), {vector->getType(), element->getType()}, false);
+          auto refLog = i->getModule()->getOrInsertFunction("_write_log", ft);
+          CallInst::Create(refLog, params, "", useInstr);
+        }
+      }
+    }
+    auto &ctx = i->getContext();
+    auto *ft = FunctionType::get(Type::getVoidTy(ctx), {target->getType(), Type::getInt64Ty(ctx)}, false);
+    return cast<Value>(i->getModule()->getOrInsertFunction("_reference_log", ft));
+  }
+  
+  
+  /**
+   Find write instructions for a given value.
+   
+   When we have a function that returns a reference to an object (e.g. operator[]/at) we
+   want to find all write instructions that write to that value's space.
+
+   @param source The source value
+   @param depth Depth limiter - how many steps should be checked
+   @return The set of values (Instructions really) that are considered to be 'write' instructions
+   */
+  std::set<Value*> findWriteInstructions(Value *source, int depth = 4) {
+    if (--depth == 0) return {};
+    std::set<Value*> result;
+    if (isa<StoreInst>(source)) result.insert(source);
+    else if (auto call = dyn_cast<CallInst>(source)) {
+      if (isa<MemCpyInst>(call)) {
+        result.insert(source);
+        return result;
+      }
+      for (auto user: source->users()) {
+        auto userInstrs = findWriteInstructions(user, depth);
+        result.insert(userInstrs.begin(), userInstrs.end());
+      }
+    } else if (isa<InvokeInst>(source)) {
+      for (auto user: source->users()) {
+        auto userInstrs = findWriteInstructions(user, depth);
+        result.insert(userInstrs.begin(), userInstrs.end());
+      }
+    } else if (auto bitcast = dyn_cast<BitCastInst>(source)) {
+      for (auto user: bitcast->users()) {
+        auto userInstrs = findWriteInstructions(user, depth);
+        result.insert(userInstrs.begin(), userInstrs.end());
+      }
+    } else {
+      errs() << "Unexpected instruction: \n";
+      source->dump();
+    }
+
+    return result;
   }
 };
 
